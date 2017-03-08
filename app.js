c4c8d0d3afe6d73c626b200f088f0c9f026c87a8
James <jamessalamonsen@gmail.com>
2017-03-08 15:32:37 -0800

    I added my ordered and un-ordred lists

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 'use strict';
 //I need to make a five yes or no question game.
+var cobalt = 'That\'s right! Colbalt was the color of James\' truck! You deserve MHANY KITTEHS!'
+var cobalt2 = 'Well.... I guess if you rememberd that the truck was blue we can let it slide.'
 var blueQuestion = prompt('Did you notce what color of blue was mentioed in the Bio?')
 if (blueQuestion.toLowerCase() === 'yes' || blueQuestion.toLowerCase() === 'y') {
-  alert('That\'s right! Colbalt was the color of James\' truck! You deserve MHANY KITTEHS!' )
+  console.log(cobalt);
+  alert(cobalt)
 }
 else if (blueQuestion.toLowerCase() === 'no' || blueQuestion.toLowerCase() === 'n') {
-  alert('Well.... I guess if you rememberd that the truck was blue we can let it slide.')
+  alert(cobalt2);
+  console.log(cobalt2)
 }
 else {
+  console.log('WHAT IS YOUR MALFUNCTION');
   alert('WHAT IS YOUR MALFUNCTION!')
 };
 
+var treeAlertYes = 'Many people like you and James have had a similar situation from having too much of a good thing.'
+var treeAlertNo = 'You must have had a good mentor to show you how to approach certain situations so to not be overwhelmed.'
 var treeQuestion = prompt('Did you ever have a hobby that was ruined for you?')
 if (treeQuestion.toLowerCase() === 'yes' || treeQuestion.toLowerCase() === 'y') {
-  alert('Many people like you and James have had a similar situation from having too much of a good thing.' )
+  alert(treeAlertYes)
+  console.log(treeAlertYes)
 }
 else if (treeQuestion.toLowerCase() === 'no' || treeQuestion.toLowerCase() === 'n') {
-  alert('You must have had a good mentor to show you how to approach certain situations so to not be overwhelmed.')
+  alert(treeAlertNo)
+  console.log(treeAlertNo)
 }
 else {
   alert('WHY ARE YOU STILL MALFUNCTIONING!!!!')
+  console.log('WHY ARE YOU STILL MALFUNCTIONING!!!')
 };
 
+var pictureAlert =
 var pictureQuestion = prompt('Do you think the background picture of the webste is photoshopped?')
 if (pictureQuestion.toLowerCase() === 'yes' || pictureQuestion.toLowerCase() === 'y') {
   alert('Well you Sir are mistaken! That is an actual plant that James helped grow.  For the past 6 years, James has been the grounds keeper as a side hustle for a very large event property in Woodinville, WA. ' )
