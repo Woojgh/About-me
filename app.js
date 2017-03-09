0a3f48bd99941315fae8972f122b541ddc409a54
James <jamessalamonsen@gmail.com>
2017-03-08 18:33:24 -0800

    I changed all of the question responses to variables and I added in console logs.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'use strict';
 //I need to make a five yes or no question game.
-var cobalt = 'That\'s right! Colbalt was the color of James\' truck! You deserve MHANY KITTEHS!'
-var cobalt2 = 'Well.... I guess if you rememberd that the truck was blue we can let it slide.'
+var cobaltQuestionYes = 'That\'s right! Colbalt was the color of James\' truck! You deserve MHANY KITTEHS!'
+var cobaltQuestionNo = 'Well.... I guess if you rememberd that the truck was blue we can let it slide.'
 var blueQuestion = prompt('Did you notce what color of blue was mentioed in the Bio?')
 if (blueQuestion.toLowerCase() === 'yes' || blueQuestion.toLowerCase() === 'y') {
-  console.log(cobalt);
-  alert(cobalt)
+  console.log(cobaltQuestionYes);
+  alert(cobaltQuestionYes)
 }
 else if (blueQuestion.toLowerCase() === 'no' || blueQuestion.toLowerCase() === 'n') {
-  alert(cobalt2);
-  console.log(cobalt2)
+  alert(cobaltQuestionNo);
+  console.log(cobaltQuestionNo)
 }
 else {
   console.log('WHAT IS YOUR MALFUNCTION');
@@ -32,36 +32,49 @@ else {
   console.log('WHY ARE YOU STILL MALFUNCTIONING!!!')
 };
 
-var pictureAlert =
+var pictureAlertYes = 'Well you Sir are mistaken! That is an actual plant that James helped grow.  For the past 6 years, James has been the grounds keeper as a side hustle for a very large event property in Woodinville, WA. '
+var pictureAlertNo = 'Nice job! That is an actual plant that James helped grow.  For the past 6 years James has been the grounds keeper as a side hustle for a very large event property in Woodinville, WA. '
 var pictureQuestion = prompt('Do you think the background picture of the webste is photoshopped?')
 if (pictureQuestion.toLowerCase() === 'yes' || pictureQuestion.toLowerCase() === 'y') {
-  alert('Well you Sir are mistaken! That is an actual plant that James helped grow.  For the past 6 years, James has been the grounds keeper as a side hustle for a very large event property in Woodinville, WA. ' )
+  alert(pictureAlertYes)
+  console.log(pictureAlertYes)
 }
 else if (pictureQuestion.toLowerCase() === 'no' || pictureQuestion.toLowerCase() === 'n') {
-  alert('Nice job! That is an actual plant that James helped grow.  For the past 6 years James has been the grounds keeper as a side hustle for a very large event property in Woodinville, WA. ')
+  alert(pictureAlertNo)
+  console.log(pictueAlertNo)
 }
 else {
   alert('WHAT IS YOUR MALFUNCTION!')
-};
+  console.log('WHAT IS YOUR MALFUNCTION!')
+}
 
-var googleQuestion = prompt('It is no secret who James wants to work for, huh?')
+var googleAlertYes = 'At one point, James worked for Microsoft in their game testing department.  He even worked on Call of Duty: Black Op\'s 2 before it hit the shelves! But alas Google still seems to be that next hurtle!'
+var googleAlertNo = 'There is a link to show what Google is all about. James sister actually worked in the Google Maps department but the first day she had to call James asking why her keyboard wasn\'t working.  Turns out her keyboard wasn\'t plugged in!'
+var googleMalfunction = 'ok.... seriously..... how...can.. you ... not ....answer YES OR NO!!!!!!!!! MALFUNCTION!!!!';
+var googleQuestion = prompt('It is no secret who James wants to work for, huh?');
 if (googleQuestion.toLowerCase() === 'yes' || googleQuestion.toLowerCase() === 'y') {
-  alert('At one point, James worked for Microsoft in their game testing department.  He even worked on Call of Duty: Black Op\'s 2 before it hit the shelves! But alas Google still seems to be that next hurtle!' )
+  alert(googleAlertYes );
 }
 else if (googleQuestion.toLowerCase() === 'no' || googleQuestion.toLowerCase() === 'n') {
-  alert('There is a link to show what Google is all about. James sister actually worked in the Google Maps department but the first day she had to call James asking why her keyboard wasn\'t working.  Turns out her keyboard wasn\'t plugged in!')
+  alert(googleAlertNo);
 }
 else {
-  alert('ok.... seriously..... how...can.. you ... not ....answer YES OR NO!!!!!!!!! MALFUNCTION!!!!')
-};
+  alert(googleMalfunction);
+  console.log(googleMalfunction);
+}
 
-var Question = prompt('Did you notce what color of blue was mentioed in the Bio?')
-if (blueQuestion.toLowerCase() === 'yes' || blueQuestion.toLowerCase() === 'y') {
-  alert('That\'s right! Colbalt was the color of James\' truck! You deserve MHANY KITTEHS!' )
+var christianQuestionYes = 'It is true. James has read through the BIble at least 5 times! too many!';
+var christianQuestionNo = 'He was rasied Baptist and went to a private christian school';
+var christianQuestion = prompt('Can you remember what religion James was raised in?');
+if (christianQuestion.toLowerCase() === 'yes' || christianQuestion.toLowerCase() === 'y') {
+  alert(christianQuestionYes );
+  console.log(christianQuestionYes);
 }
-else if (blueQuestion.toLowerCase() === 'no' || blueQuestion.toLowerCase() === 'n') {
-  alert('Well.... I guess if you rememberd that the truck was blue we can let it slide.')
+else if (christianQuestion.toLowerCase() === 'no' || christianQuestion.toLowerCase() === 'n') {
+  alert(christianQuestionNo);
+  console.log(christianQuestionNo);
 }
 else {
-  alert('WHAT IS YOUR MALFUNCTION!')
-};
+  alert('You are a loaf of BREAD!');
+  console.log('You are a loaf of BREAD!');
+}
